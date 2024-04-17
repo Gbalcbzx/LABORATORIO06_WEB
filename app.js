5ad91264186bebfe0f2352e0b9f2f74a1adcbde6
cmendoza-dev <carlos.mendoza.s@tecsup.edu.pe>
2024-04-17 17:03:38 -0500

    Service

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.static('public'));
-// Ruta para renderizar la plantilla Pug
-app.get('/pug', (req, res) => {
-  res.render('index', { name: 'Usuario Pug' });
-});
+
 
 // Configurar EJS como motor de plantillas para una ruta específica
 app.engine('ejs', require('ejs').renderFile);
@@ -20,6 +17,11 @@ app.get('/ejs', (req, res) => {
   res.render('index.ejs', { name: 'Usuario EJS' });
 });
 
+app.get('/service', (req, res) => {
+    res.render('service.ejs', { mensaje: '¡Hola desde la plantilla Service!' });
+});
+
+
 app.get('/perfil/:id', (req, res) => {
     const userId = req.params.id;
     // Aquí puedes buscar los datos del usuario en una base de datos, por ejemplo
@@ -27,17 +29,6 @@ app.get('/perfil/:id', (req, res) => {
     res.render('perfil', { user: user });
   });
 
-// Ruta para renderizar la plantilla Pug
-app.get('/miplantilla-pug', (req, res) => {
-    res.render('miplantilla', { mensaje: '¡Hola desde la plantilla Pug!' });
-  });
-  
-  // Ruta para renderizar la plantilla EJS
-  app.get('/miplantilla-ejs', (req, res) => {
-    res.render('miplantilla.ejs', { mensaje: '¡Hola desde la plantilla EJS!' });
-  });
-  
-  
 
 // Iniciar el servidor en el puerto 3000
 app.listen(3000, () => {
